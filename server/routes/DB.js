56ca2c5e547c7dc29aac78b63f4cf28efe996cdc
adityasharma0131 <adityasharma0431@gmail.com>
2024-06-09 20:52:48 +0530

    bcrypt the password stored in the database in the userModule file

--- server/routes/DB.js
+++ server/routes/DB.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const connect = mongoose.connect(
   "mongodb+srv://adityasharma0431:anant99@cluster0.rqziivy.mongodb.net/"
@@ -13,18 +14,22 @@ connect
     console.log("Error Connecting Database!");
   });
 
-const UserSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
   },
-);
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 module.exports = mongoose.model("User", UserSchema);
